28674c68a7dbd5875337c24b90acf8e5d78030fb
Ilya Sinitsa <ilya.sinitsa@yandex.ru>
2019-07-18 21:37:47 +0300

    final commit

--- server/file-server.js
+++
@@ -1,8 +0,0 @@
-const net = require('net');
-const fs = require('fs');
-
-let fileServer = net.createServer(socket => {
-
-  var chunks = [];
-
-}).listen(9967, '127.0.0.1', () => {console.log('File server is running')});
